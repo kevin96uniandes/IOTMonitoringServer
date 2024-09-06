57a64a322e8634fa38922012f4951089a87e7f2c
Kevin Alexander Maldonado Delgado <k.maldonadod@uniandes.edu.co>
2024-09-06 14:53:32 -0500

    corregir ips maquinas

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.174.147.42"]
+ALLOWED_HOSTS = ["localhost", "54.84.123.43"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.234.107.94",  # Dirección IP de la base de datos
+        "HOST": "54.80.66.27",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "98.81.193.121"
+MQTT_HOST = "54.167.253.169"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
