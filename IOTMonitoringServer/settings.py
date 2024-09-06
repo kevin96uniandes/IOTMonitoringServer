20cb800b9984f444568e026a3c92090b83e863d2
Kevin Alexander Maldonado Delgado <k.maldonadod@uniandes.edu.co>
2024-09-05 19:53:26 -0500

    cambiar host MQTTT

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "18.234.26.173"
+MQTT_HOST = "3.88.255.213"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
