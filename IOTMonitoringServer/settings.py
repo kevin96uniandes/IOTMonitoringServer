15ea6db341dbfbda4ac2cf81fed7c03210419abf
Kevin Alexander Maldonado Delgado <k.maldonadod@uniandes.edu.co>
2024-09-10 09:20:40 -0500

    añadir nuevas ip y configuracion de prueba de cron

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.234.85.100"]
+ALLOWED_HOSTS = ["localhost", "54.236.189.111"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "34.229.98.20",  # Dirección IP de la base de datos
+        "HOST": "35.175.241.133",  # Dirección IP de la base de datos
         "PORT": "5432",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.229.245.23"
+MQTT_HOST = "54.162.125.56"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
